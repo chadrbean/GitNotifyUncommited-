cc2bdc8f18b1aee8d20547e0d6acd0f9f09f4415
chad <crb4u@yaoo.com>
2023-12-03 11:47:27 -0800

    adding working files

--- src/GitNotifyUncommited.java
+++ src/GitNotifyUncommited.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class GitNotifyUncommited {
 
 
 	public static void main(String[] args) throws IOException {
